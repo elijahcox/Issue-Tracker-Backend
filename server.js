39b4308abb47ee460950eabfd7dfe664a37a1f8b
Elijah <elijah.thomas.cox@gmail.com>
2024-04-19 18:52:13 -0400

    add root routing to serve static html page

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
-
 const app = express();
 const PORT = process.env.port || 3500;
+
+app.use('/', require('./routes/root'));
 
 app.listen(PORT, ()=> console.log('Server running on port: ' + PORT))
