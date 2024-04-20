7c149a7a21758eed2da2194af891acc6d61943af
Elijah <elijah.thomas.cox@gmail.com>
2024-04-19 21:08:43 -0400

    implement logEvents with uuid and timestamp capture

--- middleware/logEvents.js
+++ middleware/logEvents.js
@@ -4,8 +4,17 @@ const fs = require('fs');
 const fsPromises = fs.promises;
 const path = require('path');
 
-const logEvents = async(message,logName) => {
-
+async function logEvents(message,logName){
+    const dateTime = format(new Date(),'yyyy-MM-dd\tHH:MM:ss');
+    const logItem = dateTime + '\t' + uuid() + message + '\n';
+    try{
+        if (!fs.existsSync(path.join(__dirname,'..',logName))){
+            await fsPromises.mkdir(path.join(__dirname,'..',logName));
+        }
+        await fsPromises.appendFile(path.join(__dirname,'..',logName),logItem);
+    }catch(error){
+        console.error(error);
+    }
 }
 
 const logger = (req,res,next)=>{
