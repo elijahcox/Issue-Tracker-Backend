cf8430fab527d71519d723de9f10d82fd9346bcf
Elijah <elijah.thomas.cox@gmail.com>
2024-04-19 21:28:07 -0400

    update eventlogger function to fix file creation error, add logger to server middleware

--- middleware/logEvents.js
+++ middleware/logEvents.js
@@ -1,17 +1,17 @@
 const {format} = require('date-fns');
 const {v4:uuid} = require('uuid');
 const fs = require('fs');
-const fsPromises = fs.promises;
+const fsPromises = require('fs').promises;//common core
 const path = require('path');
 
 async function logEvents(message,logName){
     const dateTime = format(new Date(),'yyyy-MM-dd\tHH:MM:ss');
-    const logItem = dateTime + '\t' + uuid() + message + '\n';
+    const logItem = dateTime + '\t' + uuid() + '\t' +  message + '\n';
     try{
         if (!fs.existsSync(path.join(__dirname,'..',logName))){
-            await fsPromises.mkdir(path.join(__dirname,'..',logName));
+            await fsPromises.mkdir(path.join(__dirname,'..','logs'));
         }
-        await fsPromises.appendFile(path.join(__dirname,'..',logName),logItem);
+        await fsPromises.appendFile(path.join(__dirname,'..','logs',logName),logItem);
     }catch(error){
         console.error(error);
     }
@@ -21,3 +21,5 @@ const logger = (req,res,next)=>{
     logEvents(req.method + " " + req.headers.origin + " " + req.url + "\n", 'reqLog.txt');
     next();
 }
+
+module.exports = {logEvents,logger};
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const server = express();
+const {logger} = require('./middleware/logEvents.js')
 const PORT = process.env.port || 3500;
+
+server.use(logger);//custom middleware logger
 
 server.use('/', require('./routes/root'));
 
