8adff8cb236de2ff099279a4c8879807cb89bf18
Elijah <elijah.thomas.cox@gmail.com>
2024-04-22 16:17:59 -0400

    fix logs mkdir call, add formatting due to prettier

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 const server = express();
-const {logger, errorLogger} = require('./middleware/logEvents.js')
+const { logger, errorLogger } = require("./middleware/logEvents.js");
 const PORT = process.env.port || 3500;
 
-server.use(logger);//custom middleware logger
+server.use(logger); //custom middleware logger
 
-server.use('/', require('./routes/root'));
+server.use("/", require("./routes/root"));
 
-server.use(errorLogger);//custom middleware logger
+server.use(errorLogger); //custom middleware logger
 
-server.listen(PORT, ()=> console.log('Server running on port: ' + PORT))
+server.listen(PORT, () => console.log("Server running on port: " + PORT));
 
 module.exports = server;
