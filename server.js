d7d6e21c10291813fc9724f64fe51b249516d9b0
Elijah <elijah.thomas.cox@gmail.com>
2024-04-23 14:00:45 -0400

    add path import, server.all logic for handling unexpected requests, update server.test file to test

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 const server = express();
 const { logger, errorLogger } = require("./middleware/logEvents.js");
 const connectDB = require("./config/dbConn.js");
@@ -11,6 +12,20 @@ connectDB();
 server.use(logger); //custom middleware logger
 
 server.use("/", require("./routes/root"));
+
+server.all("*", (req, res) => {
+    res.status(404); //set response status to 404
+    if (req.accepts("html")) {
+        //for browser based requests
+        res.sendFile(path.join(__dirname, "views", "404.html"));
+    } else if (req.accepts("json")) {
+        //for api calls
+        res.json({ error: "404 Not Found" });
+    } else {
+        //other
+        res.type("txt").send("404 not found");
+    }
+});
 
 server.use(errorLogger); //custom middleware logger
 
--- test/server.test.js
+++ test/server.test.js
@@ -1,15 +1,25 @@
-const request = require('supertest');
-const chai = require("fix-esm").require('chai');
-const server = require('../server');
+const request = require("supertest");
+const chai = require("fix-esm").require("chai");
+const server = require("../server");
 const expect = chai.expect;
 
-describe('GET /', () => {
-  it('should return status 200', (done) => {
-    request(server)
-      .get('/')
-      .end((err, res) => {
-        expect(res.status).to.equal(200);
-        done();
-      });
-  });
+describe("Simple Get requests", () => {
+    it("should return status 200 when GET /", (done) => {
+        request(server)
+            .get("/")
+            .end((err, res) => {
+                expect(res.status).to.equal(200);
+                done();
+            });
+    });
+
+    it("should return status 404 when GET/{undefined}", (done) => {
+        request(server)
+            .get("/junk")
+            .end((err, res) => {
+                expect(res.status).to.equal(404);
+                expect(res.text).to.contain("The requested resource could not be found");
+                done();
+            });
+    });
 });
