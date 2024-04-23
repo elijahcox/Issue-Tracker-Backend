b392674ebc5d1fdefe247781badde7897ddbf4b1
Elijah <elijah.thomas.cox@gmail.com>
2024-04-23 10:08:59 -0400

    init task schema file, add fields to user schema

--- model/User.js
+++ model/User.js
@@ -1,4 +1,19 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({});
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        required: true,
+        default: 2000,
+    },
+    refreshToken: String,
+});
