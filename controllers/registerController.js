b0466a37c03df735092e08723b2bbac8e40ca597
Elijah <elijah.thomas.cox@gmail.com>
2024-04-24 11:07:23 -0400

    finish handleNewUser function

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -2,5 +2,35 @@ const User = require("../model/User");
 const bcrypt = require("bcrypt");
 
 const handleNewUser = async (req, res) => {
-    //
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (!username || !password) {
+        //check for required present fields
+        res.status(400).json({ "error": "username and password fields are required" });
+    }
+
+    //check for duplicates
+    const duplicate = await User.findOne({ username: username }).exec();
+
+    if (duplicate) {
+        return res.sendStatus(409);
+    }
+    //create bcrypt object
+    try {
+        //encrypt password
+        const hashedPwd = await bcrypt.hash(password, 10);
+
+        //mongoose can create and store all at once
+        const result = await User.create({
+            "username": username,
+            "password": hashedPwd
+        });
+        console.log(result);
+        res.status(201).json({ "success": "New user " + user + " created" });
+    } catch (error) {
+        res.status(500).json({ "message": error.message });
+    }
 };
+
+module.exports = handleNewUser;
