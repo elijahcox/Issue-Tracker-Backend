6687648bbb81e604da9a11c74acab7813de75634
Elijah <elijah.thomas.cox@gmail.com>
2024-04-24 12:54:10 -0400

    remove comments

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -6,22 +6,17 @@ const handleNewUser = async (req, res) => {
     const password = req.body.password;
 
     if (!username || !password) {
-        //check for required present fields
         res.status(400).json({ "error": "username and password fields are required" });
     }
 
-    //check for duplicates
     const duplicate = await User.findOne({ username: username }).exec();
 
     if (duplicate) {
         return res.sendStatus(409);
     }
-    //create bcrypt object
     try {
-        //encrypt password
         const hashedPwd = await bcrypt.hash(password, 10);
 
-        //mongoose can create and store all at once
         const result = await User.create({
             "username": username,
             "password": hashedPwd
