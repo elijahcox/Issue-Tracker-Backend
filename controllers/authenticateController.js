aa67b58d1c4d0409f8b65dbf4d89bf1949a422af
Elijah <elijah.thomas.cox@gmail.com>
2024-04-26 12:51:34 -0400

    implement password validation via bcrypt,  jwt access and refresh logic

--- controllers/authenticateController.js
+++ controllers/authenticateController.js
@@ -1,4 +1,6 @@
 const User = require("../model/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const handleLogin = async (req, res) => {
     const username = req.body.username;
@@ -8,8 +10,44 @@ const handleLogin = async (req, res) => {
         res.status(400).json({ "message": "username and password required" });
     }
 
-    const foundUser = User.findOne({ username: username });
-    console.log(foundUser);
+    const foundUser = await User.findOne({ username: username }).exec();
+
+    if (!foundUser) {
+        return res.sendStatus(401);
+    }
+
+    try {
+        const match = await bcrypt.compare(password, foundUser.password);
+        if (match) {
+            const role = foundUser.role;
+            const accessToken = jwt.sign(
+                { "UserInfo": { "username": foundUser.username, "role": role } },
+                process.env.ACCESS_TOKEN_SECRET,
+                { expiresIn: "2m" }
+            ); //5-15min in prod
+
+            const refreshToken = jwt.sign(
+                { "username": foundUser.username },
+                process.env.REFRESH_TOKEN_SECRET,
+                { expiresIn: "1d" }
+            );
+
+            const result = await User.updateOne(
+                { username: username },
+                { refreshToken: refreshToken }
+            );
+            res.cookie("jwt", refreshToken, {
+                httpOnly: true,
+                maxAge: 24 * 60 * 60 * 1000,
+                sameSite: "None" /*,secure:true*/
+            }); //set http only refresh token for security
+            res.status(200).json({ accessToken });
+        } else {
+            return res.sendStatus(401);
+        }
+    } catch (error) {
+        return res.status(500).json({ "message": error.message });
+    }
 };
 
 module.exports = { handleLogin };
