8174f2c73df62956e5aa9e643ba3415f56bbace8
Elijah <elijah.thomas.cox@gmail.com>
2024-04-26 12:19:07 -0400

    init auth controller, add route to server, create route file

---
+++ routes/api/authenticate.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const authenticateController = require("../../controllers/authenticateController");
+
+router.route("/").post(authenticateController.handleLogin);
+
+module.exports = router;
