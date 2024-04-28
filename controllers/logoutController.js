39f79de84056dd27d0de6fd394880cf4dea02f8a
Elijah Cox <elijah.thomas.cox@gmail.com>
2024-04-28 11:21:24 -0500

    add comment flow to logout

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -1,3 +1,10 @@
-const handleLogout = async (req, res) => {};
+const User = require("../model/User");
+
+const handleLogout = async (req, res) => {
+    //get cookie, check if token present in cookie
+    //if no token in cookie, return success status
+    //if token in cookie, look up user in DB based on cookie
+    //clear from cookie and user, save and return success status
+};
 
 module.exports = { handleLogout };
