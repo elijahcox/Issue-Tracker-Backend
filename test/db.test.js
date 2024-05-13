04a3076b0f47ce0f304d790c468b57ad58f73e1e
Elijah Cox <elijah.thomas.cox@gmail.com>
2024-05-13 11:56:14 -0500

    create uid, tid, and positive test to create a task, add user db checking to older register test

--- test/db.test.js
+++ test/db.test.js
@@ -3,6 +3,7 @@ const chai = require("fix-esm").require("chai");
 const server = require("../server");
 const connectDB = require("../config/dbConn.js");
 const User = require("../model/User.js");
+const Task = require("../model/Task.js");
 const expect = chai.expect;
 
 const tempUser = {
@@ -10,21 +11,33 @@ const tempUser = {
     password: "testPassword"
 };
 
-describe("Register User", () => {
+const tempTask = {
+    title: "Test Task",
+    size: "3",
+    priority: "small"
+};
+
+let accessToken;
+let uid;
+let tid;
+
+describe("Standard CRUD Lifecycle Flow for Users and Tasks", () => {
     before(async () => {
         await connectDB();
     });
 
-    it("should create a test user when posting JSON to /register", (done) => {
-        request(server)
+    it("should create a test user when posting JSON to /register", async () => {
+        await request(server)
             .post("/register")
             .send(tempUser)
             .expect(201)
             .then((res) => {
                 expect(res.body.username).to.be.eql(tempUser.username);
-                done();
             })
             .catch((err) => done(err));
+        const foundUser = await User.findOne({ username: tempUser.username }).exec();
+        expect(foundUser).to.not.eql(null);
+        uid = foundUser._id.toString();
     });
 
     it("should return an access token in body and upload refresh to DB when posting valid JSON credentials to /authenticate", async () => {
@@ -34,9 +47,24 @@ describe("Register User", () => {
             .expect(200)
             .then((res) => {
                 expect(res.body.accessToken).to.not.eql("");
+                accessToken = res.body.accessToken;
             });
         const foundUser = await User.findOne({ username: tempUser.username }).exec();
         expect(foundUser.refreshToken).to.not.eql(null);
+    });
+
+    it("should create a new task when posting a task to /tasks with AT in body", async () => {
+        await request(server)
+            .post("/tasks")
+            .send(tempTask)
+            .set("Authorization", "Bearer " + accessToken)
+            .expect(201)
+            .then((res) => {
+                tid = res.body._id;
+                expect(res.body).to.not.eql("");
+            });
+        const foundTask = await Task.findOne({ _id: tid, userID: uid }).exec();
+        expect(foundTask).to.not.eql(null);
     });
 
     it("should clear a refresh token from cookie and user database upon request to /logout", (done) => {
